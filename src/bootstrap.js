6f4919ec2d04b783a82bf1a0d48c7d834be38a43
Nathan Lamb <usmc_wolf@hotmail.com>
2022-08-16 17:37:34 -0500

    added comment

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,10 +1,8 @@
 import moment from "moment";
 
 console.log(moment.now());
-const rightNow = moment();
-
-console.log(rightNow);
 
 const birthday = moment("1983-04-18", "YYYY-MM-DD");
 
 console.log(birthday.fromNow());
+//   sending change to github
